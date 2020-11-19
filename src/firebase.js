1f70989365e30cdcb19d4f41e3100869c7d9c034
Vanshika <vanshika31032000@gmail.com>
2020-11-19 20:44:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSlWAqbfbzPWwL1spsDzc6FvON2C0xzhw',
-  authDomain: 'react-slack-clone-cfb67.firebaseapp.com',
-  databaseURL: 'https://react-slack-clone-cfb67.firebaseio.com',
-  projectId: 'react-slack-clone-cfb67',
-  storageBucket: 'react-slack-clone-cfb67.appspot.com',
-  messagingSenderId: '591165248580',
-  appId: '1:591165248580:web:bead84beef29449ecd1811',
+
 };
 
 // Initialize Firebase
